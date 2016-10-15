1b2f627d7025b07ccbee97119005d45b1cc2e6d5
Dean <deanfreekinggalvin@gmail.com>
2016-10-15 15:40:22 -0400

    Updated to 0.14.14

--- new_smart_launch.sh
+++ new_smart_launch.sh
@@ -100,7 +100,7 @@ cat << EOF > $SERVER_SETTINGS
 "game_password": "$FACTORIO_SERVER_GAME_PASSWORD",
 
 "_comment_verify_user_identity": "When set to true, the server will only allow clients that have a valid Factorio.com account",
-"verify_user_identity": $FACTORIO_SERVER_VERIFY_IDENTITY
+"verify_user_identity": "$FACTORIO_SERVER_VERIFY_IDENTITY"
 }
 EOF
 # Setting heavy mode option
@@ -114,16 +114,16 @@ then
 factorio_command="$factorio_command --complete"
 fi
 # Setting allow-commands option
-factorio_command="$factorio_command --allow-commands $FACTORIO_ALLOW_COMMANDS"
+#factorio_command="$factorio_command --allow-commands $FACTORIO_ALLOW_COMMANDS"
 # Setting auto-pause option
-if [ "$FACTORIO_NO_AUTO_PAUSE" == true ] 
+if [ "$FACTORIO_NO_AUTO_PAUSE" == true ]
 then
 factorio_command="$factorio_command --no-auto-pause"
 fi
 # Setting autosave-interval option
-factorio_command="$factorio_command --autosave-interval $FACTORIO_AUTOSAVE_INTERVAL"
+#factorio_command="$factorio_command --autosave-interval $FACTORIO_AUTOSAVE_INTERVAL"
 # Setting autosave-slots option
-factorio_command="$factorio_command --autosave-slots $FACTORIO_AUTOSAVE_SLOTS"
+#factorio_command="$factorio_command --autosave-slots $FACTORIO_AUTOSAVE_SLOTS"
 # Setting rcon-port option
 factorio_command="$factorio_command --rcon-port 27015"
 # Setting rcon password option
